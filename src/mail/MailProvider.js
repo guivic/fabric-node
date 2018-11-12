83afa8fe0ca474d8bb8afbbc0a28a9c008c7e7f9
JrmyB <bljrmy@gmail.com>
2018-11-12 09:25:25 +0100

    attachments to mails

--- src/mail/MailProvider.js
+++ src/mail/MailProvider.js
@@ -1,10 +1,11 @@
 const Joi = require('joi');
 
 const mailOptionsSchema = Joi.object().keys({
-	from:    Joi.string().email().required(),
-	to:      Joi.string().email().required(),
-	subject: Joi.string().required(),
-	text:    Joi.string().required(),
+	from:					Joi.string().email().required(),
+	to:      			Joi.string().email().required(),
+	subject: 			Joi.string().required(),
+	text:    			Joi.string().required(),
+	attachments:	Joi.array()
 });
 
 /**
